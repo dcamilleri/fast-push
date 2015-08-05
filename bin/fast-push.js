daec9efe801cbb732f75c0d9ec4d81bad424a982
dcamilleri <dorian.camilleri@yahoo.fr>
2015-08-05 11:47:39 -0400

    Push origin head

--- bin/fast-push.js
+++ bin/fast-push.js
@@ -2,22 +2,19 @@
 
 var execPromise = require('../utils/exec-promise');
 var errorLogs = require('../utils/errors');
-var currentBranch;
+
+console.log('Without getting branch----------');
 
 function fastPush(commitMessage) {
   execPromise('git --version', errorLogs.gitNotInstalled)
     .then(function() {
-      return execPromise('git rev-parse --abbrev-ref HEAD', errorLogs.branchNotFound);
-    })
-    .then(function(branch) {
-      currentBranch = branch;
       return execPromise('git add --all', errorLogs.addError);
     })
     .then(function() {
       return execPromise('git commit -m "' + commitMessage + '"', errorLogs.commitError);
     })
     .then(function() {
-      return execPromise('git push origin ' + currentBranch, errorLogs.commitError);
+      return execPromise('git push origin HEAD', errorLogs.commitError);
     })
     .then(function() {
       console.log('"' + commitMessage + '"\nPushed!');
--- utils/errors.js
+++ utils/errors.js
@@ -2,5 +2,5 @@ module.exports = {
   gitNotInstalled: 'You don\'t have git installed',
   branchNotFound: 'Can\'t get your current branch. Are you sure this is a git repository?',
   addError: 'Staging failed. Check your staged files',
-  commitError: 'Commit failed. Have you staged all your files ?'
+  commitError: 'Commit failed. Have you staged all your files ?\nAlso try to git pull before pushing.'
 };
